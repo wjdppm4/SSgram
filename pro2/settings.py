4d7c42e3f82a7620a5c9df22d32e77ac82a1016f
OH <wjdppm4@gmail.com>
2021-06-14 17:04:40 +0900

    fix

--- pro2/settings.py
+++ pro2/settings.py
@@ -123,4 +123,11 @@ STATIC_URL = '/static/'
 
 # Channels
 ASGI_APPLICATION = 'pro2.routing.application'
-
+# CHANNEL_LAYERS = {
+#    'default': {
+#        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+#        'CONFIG' : {
+#            "hosts": [('127.0.0.1', 6379)],
+#         },
+#     },
+# }
